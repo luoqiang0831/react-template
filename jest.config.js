249e6844cfef34f53b988e183a6ff46dad574464
luoqiang <luoqiang0831@vip.qq.com>
2022-06-22 11:25:55 +0800

    feat: fileTransformer

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest/dist",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/fileTransformer.js",
+      "<rootDir>/__mocks__/fileTransformer.js",
     // "\\.svg$": "svg-jest",
   },
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$", "/antd-mobile/"],
